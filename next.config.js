ac5c535d3a8a8b3c64f37aefb9add8c95c2a2d43
Gnak <523748995@qq.com>
2024-08-14 10:30:07 +0800

    feat: 修改基础配置,增加dockerfile

--- next.config.js
+++ next.config.js
@@ -54,7 +54,7 @@ const securityHeaders = [
   },
 ]
 
-const output = process.env.EXPORT ? 'export' : undefined
+const output = process.env.EXPORT ? 'standalone' : 'standalone'
 const basePath = process.env.BASE_PATH || undefined
 const unoptimized = process.env.UNOPTIMIZED ? true : undefined
 
